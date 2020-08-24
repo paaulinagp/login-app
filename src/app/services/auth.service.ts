ff350f1e271a5351ff1fa099d10b5e3391e704f4
Paulina Guerrero <paaulinagp@gmail.com>
2020-08-24 00:22:54 -0500

    feat: guardando token de la sesion del usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { UserModel } from "../models/user.model";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -9,6 +10,7 @@ import { Observable } from "rxjs";
 export class AuthService {
   private url = "https://identitytoolkit.googleapis.com/v1/accounts:";
   private APIKEY = "AIzaSyB23l41CkGiYqNMqkBFp0J1ZEJOYn5utnk";
+  token: string;
 
   constructor(private http: HttpClient) {}
 
@@ -17,10 +19,14 @@ export class AuthService {
       ...user,
       returnSecureToken: true,
     };
-    return this.http.post(
-      `${this.url}signInWithPassword?key=${this.APIKEY}`,
-      authData
-    );
+    return this.http
+      .post(`${this.url}signInWithPassword?key=${this.APIKEY}`, authData)
+      .pipe(
+        map((res: any) => {
+          this.setToken(res.idToken);
+          return res;
+        })
+      );
   }
 
   newUser(user: UserModel): Observable<any> {
@@ -28,8 +34,30 @@ export class AuthService {
       ...user,
       returnSecureToken: true,
     };
-    return this.http.post(`${this.url}signUp?key=${this.APIKEY}`, authData);
+    return this.http
+      .post(`${this.url}signUp?key=${this.APIKEY}`, authData)
+      .pipe(
+        map((res: any) => {
+          this.setToken(res.idToken);
+          return res;
+        })
+      );
   }
 
   logout(): void {}
+
+  private setToken(token: string) {
+    this.token = token;
+    localStorage.setItem("token", this.token);
+  }
+
+  getToken() {
+    if (localStorage.getItem("token")) {
+      this.token = localStorage.getItem("token");
+    } else {
+      this.token = "";
+    }
+
+    return this.token;
+  }
 }
