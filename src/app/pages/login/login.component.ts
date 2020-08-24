25147989feaf6e75859c3e76cd27cd68329a8583
Paulina Guerrero <paaulinagp@gmail.com>
2020-08-23 23:18:48 -0500

    feat: agregando formulario en login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { UserModel } from "src/app/models/user.model";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  user: UserModel;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.user = new UserModel();
   }
 
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      console.log("Es inválido");
+      return;
+    }
+    console.log("se envia formulario: ", form);
+  }
 }
